05961b36a1b25a8c67831bdaef0cad8e95e34901
Enochneier <enochneier@gmail.com>
2023-07-03 04:02:44 -0700

    corrections

--- main.h
+++ main.h
@@ -14,6 +14,7 @@ typedef struct print
 	int (*f)(va_list);
 } print_t;
 
+int (*check_format(const char *format))(va_list);
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_c(va_list c);
@@ -40,4 +41,4 @@ int print_less(va_list less);
 int print_space(va_list space);
 int print_sharp(va_list sharp);
 
-#endif
+#endif /* _MAIN_H */
